1a4117c168120d9f15ade9f19d2fcf1a0199c482
chris.chiasson <chris@chiasson.name>
2006-08-22 16:47:25 +0000

    first export of a figure linked with an equation - addition of namespace and version to figure elements

--- EngineeringOptimization/Documentation/English/TestHarness/TestHarness.m
+++ EngineeringOptimization/Documentation/English/TestHarness/TestHarness.m
@@ -4,14 +4,14 @@ BeginPackage["TestHarness`",{"EngineeringOptimization`","XML`DocBook`"}];
 
 (*SetOptions[XMLDocument,ConversionOptions->{}];*)
 
-exportDirectory=ToFileName[{DirectoryName[First@FileNames[Last@StringSplit[$Input,
-	$PathnameSeparator|"/"],$Path,2],2],"mout"}];
+exportDirectory=ToFileName[{DirectoryName[First@FileNames[Last@StringSplit[
+	$Input,$PathnameSeparator|"/"],$Path,2],2],"mout"}];
 
 If[FileType@exportDirectory===None,CreateDirectory@exportDirectory;
 	If[FileType@exportDirectory===None,Print["The destination directory can't \
 be created."];Quit[]]];
 
-(*xpr[1]:=DocBookInlineEquation["xpr1",1+as==b,SetIdAttribute->False];
+xpr[1]:=DocBookInlineEquation["xpr1",1+as==b,SetIdAttribute->False];
 
 xpr[2]:=XMLChain[Hold@XMLElement["para",{},{"Here is some text and an \
 equation: ",ToXML@DocBookInlineEquation["xpr2",1+as==Sqrt[b],SetIdAttribute->
@@ -20,10 +20,17 @@ equation: ",ToXML@DocBookInlineEquation["xpr2",1+as==Sqrt[b],SetIdAttribute->
 xpr[3]:=DocBookEquation["xpr3",xpr[1],1+as==2 Sqrt[b],Caption->xpr[2]];
 
 xpr[4]:=XMLDocument["test_equation1.xml",xpr[3],PrependDirectory->
-	exportDirectory];*)
+	exportDirectory];
 
 xpr[5]:=XMLDocument["test_equation1.xml",DocBookEquation["","Eqn Title",1+as==2*
-	Sqrt[b],Caption->"Caption text",SetIdAttribute->False],PrependDirectory->
+	Sqrt[b]/Pi,Caption->"Caption text",SetIdAttribute->False],PrependDirectory->
 		exportDirectory];
+
+xpr[6]:=DocBookFigure["llamaId","llama title","llama description",
+	Plot[x^(1/3),{x,0,3}],Caption->XMLChain[Hold@XMLElement["para",{},{
+		"llama caption with this equation: ",ToXML@xpr[1]}]]];
+
+xpr[7]:=XMLDocument["test_equation2.xml",xpr[6],PrependDirectory->
+	exportDirectory];
 
 EndPackage[];
