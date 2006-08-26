e516cd5fa59f61538a6a4eddf18f193e8d93513e
chris.chiasson <chris@chiasson.name>
2006-08-26 05:52:38 +0000

    commit before attempt at multi-expression equation export - \[InvisibleApplication] removal

--- EngineeringOptimization/Documentation/Documentation.m
+++ EngineeringOptimization/Documentation/Documentation.m
@@ -1,13 +1,16 @@
 BeginPackage["EngineeringOptimization`Documentation`",
-	{"EngineeringOptimization`","XML`DocBook`"}];
+	{"EngineeringOptimization`","XML`DocBook`","DrawGraphics`DrawingMaster`"}];
 
-exportDirectory=ToFileName[{DirectoryName[First@FileNames[Last@StringSplit[
-	$Input,$PathnameSeparator|"/"],$Path,3],1],"mout"}];
+EODExportDirectory::usage="This is the export directory for the Engineering \
+Optimization Documentaiton."
 
 Begin["`Private`"];
 
-If[FileType@exportDirectory===None,CreateDirectory@exportDirectory;
-	If[FileType@exportDirectory===None,Print["The destination directory can't \
+EODExportDirectory=ToFileName[{DirectoryName[First@FileNames[Last@StringSplit[
+	$Input,$PathnameSeparator|"/"],$Path,3],1],"mout"}];
+
+If[FileType@EODExportDirectory===None,CreateDirectory@EODExportDirectory;
+	If[FileType@EODExportDirectory===None,Print["The destination directory can't \
 be created."];Quit[]]];
 
 End[];
--- EngineeringOptimization/Documentation/HW1/HW1.m
+++ EngineeringOptimization/Documentation/HW1/HW1.m
@@ -1,15 +1,15 @@
-(*Begin["`HW1`"];*)
+Begin["`HW1`"];
 
 (*set NHoldAll and argument subscript formatting for X and Y*)
 
 {Attributes[#]={NHoldAll},Format[#[i:__Integer|__Symbol]]=Subscript[#,i]}&/@
-	{X,Y};
+	{X,Y,\[Lambda],h};
 
 eqn[1][X_,Y_]=(X/2)^2+Y^2==4;
 
 export[1]=XMLDocument["hw_1_ellipse.xml",
 	DocBookEquation["hw_1_ellipse","Ellipse",HoldForm[(X/2)^2+Y^2==4]],
-	PrependDirectory->exportDirectory
+	PrependDirectory->EODExportDirectory
 	];
 
 spacemapping=Transpose@{
@@ -23,9 +23,70 @@ export[2]=XMLDocument["hw_1_spacemapping.xml",DocBookTable["spacemapping",
 variables in the left column and optimization variables in the right column",
 spacemapping,TitleAbbrev->"Variable Mapping",Caption->"The rows show an \
 equivalence between a given geometric variable on the left and an optimization \
-variable on the right."],PrependDirectory->exportDirectory];
+variable on the right."],PrependDirectory->EODExportDirectory];
 
+rep[1]={pt[i_Integer]:>{X[C][i],Y[C][i],0}};
 
+rep[2]=Drop[Rule@@@spacemapping,1];
 
+disp[1]=pt[2]-pt[1];
 
-(*End[];*)
+disp[2]=pt[4]-pt[1];
+
+(*mag just stands for magnitude rather than vector - it doesn't indicate the
+absolute value operation - it doesn't really matter in this case, but we usually
+minimize the negative of the function we'd like to maximize - if the points are
+numbered in counter clockwise direction, then this will produce a negative F
+for a positive area*)
+
+fmag=F==-Last[Cross[disp[1],disp[2]]/.rep[1]/.rep[2]];
+
+export[3]=XMLDocument["hw_1_fmag.xml",DocBookEquation["hw_1_fmag",
+	"Objective Function",fmag,Caption->"The objective function is the negative \
+of the area if the points increase in consecutive numbering in the \ 
+counter-clockwise direction"],PrependDirectory->EODExportDirectory];
+
+rep[3]=Equal[a_,b_]->b-a;
+
+(*all four points must touch the ellipse*)
+
+ellipseconstraints=ReleaseHold@Table[{eqn[1][X[C][i],Y[C][i]]/.rep[1]/.rep[2]
+	/.rep[3],Hold[i]==i},{i,4}];
+
+(*the rectangle must be defined by two perpendicular sides*)
+
+rectangleconstraint={Dot[disp[1],disp[2]]==0/.rep[1]/.rep[2]/.rep[3],i==5};
+
+(*the third point must be the vector addition of displacements from the first
+point*)
+
+displacementconstraints=MapIndexed[{#,i==First[#2]+5}&,
+	Drop[Thread[
+		pt[1]+disp[1]+disp[2]==pt[3]/.rep[1]/.rep[2]/.rep[3]
+		],-1]
+	];
+
+(*in the language of optimization, these contraints are equality constraints, 
+h[i] that are required must be zero*)  
+
+constraints={Sequence@@ellipseconstraints,rectangleconstraint,
+	Sequence@@displacementconstraints};
+
+hconstraints=h[i]==0==Piecewise[constraints];
+
+export[4]=XMLDocument["hw_1_hconstraints.xml",DocBookEquation[
+	"hw_1_hconstraints","Objective Function Equality Constraints",hconstraints,
+	TitleAbbrev->"Equality Constraints"],PrependDirectory->EODExportDirectory];
+
+(*lagrange multipliers*) 
+
+lagrangemultipliers=Del[F[X[O][j]]]==HoldForm[Sum[\[Lambda][i]*
+	Del[h[i][X[O][j]]],{i,7}]];
+
+export[5]=XMLDocument["hw_1_lagrangemultipliers.xml",DocBookEquation[
+	"hw_1_lagrangemultipliers","Lagrange Multipliers",lagrangemultipliers],
+	PrependDirectory->EODExportDirectory];
+
+(*Export@@@export[#]&/@Range[5];*)
+
+End[];
