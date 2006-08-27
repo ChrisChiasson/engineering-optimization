f9cc526eb11a1204d2c07a28eae2a05334a85283
chris.chiasson <chris@chiasson.name>
2006-08-27 01:25:22 +0000

    EODExport boolean added to conditionally export files

--- EngineeringOptimization/Documentation/Documentation.m
+++ EngineeringOptimization/Documentation/Documentation.m
@@ -1,17 +1,22 @@
 BeginPackage["EngineeringOptimization`Documentation`",
 	{"EngineeringOptimization`","XML`DocBook`","DrawGraphics`DrawingMaster`"}];
 
+EODExport::usage"EODExport is a boolean variable that indicates wether the \
+documentation should be exported";
+
 EODExportDirectory::usage="This is the export directory for the Engineering \
 Optimization Documentaiton."
 
 Begin["`Private`"];
 
+EODExport=True;
+
 EODExportDirectory=ToFileName[{DirectoryName[First@FileNames[Last@StringSplit[
 	$Input,$PathnameSeparator|"/"],$Path,3],1],"mout"}];
 
 If[FileType@EODExportDirectory===None,CreateDirectory@EODExportDirectory;
-	If[FileType@EODExportDirectory===None,Print["The destination directory can't \
-be created."];Quit[]]];
+	If[FileType@EODExportDirectory===None,Print["The destination directory \
+can't be created."];Quit[]]];
 
 End[];
 
