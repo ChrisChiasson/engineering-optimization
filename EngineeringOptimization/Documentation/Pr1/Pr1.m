da686d0af9f90bd86f9bdf77453a6e9b3a46ef26
chris.chiasson <chris@chiasson.name>
2006-09-09 19:23:51 +0000

    added some definitions of variables with units

--- EngineeringOptimization/Documentation/Pr1/Pr1.m
+++ EngineeringOptimization/Documentation/Pr1/Pr1.m
@@ -11,7 +11,14 @@ MakeBoxes[SectionModulus,_]="I";
 
 MakeBoxes[YoungsModulus,_]="E";
 
-H=25;
+cm=Centi Meter;
+
+Format[F[a_]]=Subscript[F,a];
+
+rep[1]={H->25``1 cm,L->250``1 cm,A->25``1 cm^2,SectionModulus->750 cm^4,
+	YoungsModulus->70``0 GPa};
+
+eqn[1]=F[1]==A YoungsModulus (1-Sqrt[1+(X^2-2 H X)/L^2]);
 
 End[];
 
