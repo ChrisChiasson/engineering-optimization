419058b0914530c08a1ed24b2ddb06957c899b57
chris.chiasson <chris@chiasson.name>
2006-09-09 19:59:09 +0000

    added eqn[2]

--- EngineeringOptimization/Documentation/Pr1/Pr1.m
+++ EngineeringOptimization/Documentation/Pr1/Pr1.m
@@ -11,14 +11,22 @@ MakeBoxes[SectionModulus,_]="I";
 
 MakeBoxes[YoungsModulus,_]="E";
 
+MakeBoxes[Meter,_]="m";
+
+MakeBoxes[Newton,_]="N";
+
 cm=Centi Meter;
 
 Format[F[a_]]=Subscript[F,a];
 
 rep[1]={H->25``1 cm,L->250``1 cm,A->25``1 cm^2,SectionModulus->750 cm^4,
-	YoungsModulus->70``0 GPa};
+	YoungsModulus->70``0 Giga Pascal};
+
+rep[2]={Centi->1/100,Giga->10^9,Pascal->Newton/Meter^2};
 
 eqn[1]=F[1]==A YoungsModulus (1-Sqrt[1+(X^2-2 H X)/L^2]);
+
+eqn[2]=F[cr]==Pi^2*YoungsModulus*SectionModulus/L^2;
 
 End[];
 
