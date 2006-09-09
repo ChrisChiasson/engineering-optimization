0ae6ad20730d540d427319b2189d24591aa05c15
chris.chiasson <chris@chiasson.name>
2006-09-09 15:24:58 +0000

    test init file

--- EngineeringOptimization/Kernel/init.m
+++ EngineeringOptimization/Kernel/init.m
@@ -1,7 +1,1 @@
-(* Mathematica Init File *)
-
-(*uncomment the Get of the cubicCriticalDomainLocationsSource file to append a
-new defintion of cubicCriticalDomainLocationsSource to the end of the
-cubicCriticalDomainLocations.m file*)
-(*Get["EngineeringOptimization`cubicCriticalDomainLocationsSource`"]*)
-Get["EngineeringOptimization`EngineeringOptimization`"]
+Get["EngineeringOptimization`Test`Test"]
