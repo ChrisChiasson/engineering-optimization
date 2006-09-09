c5c6e9dd50d29eca5b8091e3a4c2df38be9f4e18
chris.chiasson <chris@chiasson.name>
2006-09-09 15:36:06 +0000

    the last commit was an error - rolling back

--- EngineeringOptimization/Kernel/init.m
+++ EngineeringOptimization/Kernel/init.m
@@ -1,1 +1,7 @@
-Get["EngineeringOptimization`Test`Test"]
+(* Mathematica Init File *)
+
+(*uncomment the Get of the cubicCriticalDomainLocationsSource file to append a
+new defintion of cubicCriticalDomainLocationsSource to the end of the
+cubicCriticalDomainLocations.m file*)
+(*Get["EngineeringOptimization`cubicCriticalDomainLocationsSource`"]*)
+Get["EngineeringOptimization`EngineeringOptimization`"]
