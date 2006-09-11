91ae02f862b7df0c1234c0a819cbf1608eb96ece
chris.chiasson <chris@chiasson.name>
2006-09-11 18:43:10 +0000

    I am moving the styling and transformation files to the mathematica-docbook-export project.

--- EngineeringOptimization/Documentation/js/get_firefox_with_google_toolbar.js
+++
@@ -1,6 +0,0 @@
-google_ad_client = "pub-9913151546120299";
-google_ad_width = 120;
-google_ad_height = 240;
-google_ad_format = "120x240_as_rimg";
-google_cpa_choice = "CAAQ0eSZzgEaCP5Y1BMpe0XwKPG193M";
-google_ad_channel = "";
