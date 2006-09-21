2be3593b04fa7614089dfb066da9fac3e4080f96
chris.chiasson <chris@chiasson.name>
2006-09-21 08:38:51 +0000

    a few more updates from the original source

--- EngineeringOptimization/Documentation/PR1/PR1.m
+++ EngineeringOptimization/Documentation/PR1/PR1.m
@@ -1,26 +1,53 @@
+(* ::Package:: *)
+
 BeginPackage["EngineeringOptimization`Documentation`PR1`",
-	{"EngineeringOptimization`"}];
+	{"EngineeringOptimization`",
+		"EngineeringOptimization`Documentation`"}
+	];
 
 Begin["`Private`"];
 
-request[1]:=FindMinimum[(x-10)^2,{x,1},Method->{"Unimodal",
-	"MaxNarrowingIterations"->30},StepMonitor:>Print[{"step",x}],
-	EvaluationMonitor:>Print[{"eval",x}]];
+(*requested f[x]=(x-10)^2 example*)
 
-MakeBoxes[SectionModulus,_]="I";
+request[1]=FindMinimum[(x-10)^2,{x,1},Method->{"Unimodal",
+	"MaxNarrowingIterations"->30},StepMonitor:>Sow[{"step",x}],
+	EvaluationMonitor:>Sow[{"eval",x}]];
 
-MakeBoxes[YoungsModulus,_]="E";
+(*perhaps show how the options change the results,
+as you did with the old code*)
 
-MakeBoxes[Meter,_]="m";
+(*Book Example Problem 2-1
+Numerical Optimization Techniques for
+Engineering Design
+by Garret Vanderplaats*)
 
-MakeBoxes[Newton,_]="N";
+(MakeBoxes[#1,_]=#2)&@@@
+	{{SectionModulus,"I"},
+		{YoungsModulus,"E"},
+		{Meter,"m"},
+		{Newton,"N"}
+		};
 
-cm=Centi Meter;
+$Assumptions=#>0&/@{Newton,X,Meter,YoungsModulus,SectionModulus};
 
-Format[F[a_]]=Subscript[F,a];
+$UnitList={{X,Meter},{F,Newton},{P,Newton}};
 
-rep[1]={H->25``1 cm,L->250``1 cm,A->25``1 cm^2,SectionModulus->750 cm^4,
-	YoungsModulus->70``0 Giga Pascal};
+deUnitizeVariablesRep=#->Times[##]&@@@$UnitList;
+
+unitizeVariablesRep=#->Divide[##]&@@@$UnitList;
+
+dropSIUnitsRep={Meter->1,Newton->1};
+
+numbersToMachinePrecisionRep=x_Real:>SetPrecision[x,MachinePrecision];
+
+cm=Centi*Meter;
+
+Function[Format[#[a_]]=Subscript[#,a]]/@{F};
+
+(*given values*)
+
+rep[1]={H->25 cm, L->250 cm, A->25 cm^2, SectionModulus->750 cm^4,
+	YoungsModulus->70 Giga Pascal};
 
 rep[2]={Centi->1/100,Giga->10^9,Pascal->Newton/Meter^2};
 
@@ -28,7 +55,33 @@ eqn[1]=F[1]==A YoungsModulus (1-Sqrt[1+(X^2-2 H X)/L^2]);
 
 eqn[2]=F[cr]==Pi^2*YoungsModulus*SectionModulus/L^2;
 
+eqn[3]=F==Min[eqn[1][[2]],eqn[2][[2]]];
+
+eqn[4]=P==(H-X)*F/Sqrt[L^2+X^2-2*H*X];
+
+(*Define the function to maximize.*)
+
+beamLoad[X_]=eqn[4][[2]]/.Rule@@eqn[3]/.rep[1]/.rep[2]//PiecewiseExpand;
+
+(*Plot the function and the 20000 N load line.*)
+
+gr[1]=Plot[{beamLoad[X]/.dropSIUnitsRep,20000},
+	{X,0,25/100},
+	PlotStyle->{Black,Red},
+	AxesLabel->{
+		SequenceForm[X," ","(",Meter,")"],
+		SequenceForm[P[X]," ","(",Newton,")"]
+		}
+	];
+
+(*Solve the problem in the standard Mathematica way.*)
+
+xpr[1]={beamLoad[X],0<X<=H/.rep[1]/.rep[2]}/.dropSIUnitsRep;
+
+(*This is the maximum load given to 10 digits of precision.*)
+
+
+
 End[];
 
 EndPackage[];
-
