66e21ea400f3b43fdae6f0229bcc84376a836ddd
chris.chiasson <chris@chiasson.name>
2006-09-25 12:11:40 +0000

    these testing expressions all work

--- EngineeringOptimization/Documentation/HW2/HW2.m
+++ EngineeringOptimization/Documentation/HW2/HW2.m
@@ -8,6 +8,22 @@ Begin["`Private`"];
 
 eqn[1]=F==X[1]+X[1]^2-X[2]-3*X[1]*X[2]+4*X[2]^2;
 
+FindMinimum[eqn[1][[2]],{{X[1],0},{X[2],0}}];
+
+FindMinimum[eqn[1][[2]],{{X[1],0},{X[2],0}},Method->"VariableMetric"];
+
+FindMinimum[eqn[1][[2]],{{X[1],0},{X[2],0}},Method->"SteepestDescent"];
+
+FindMinimum[eqn[1][[2]],{{X[1],0},{X[2],0}},Method->"FletcherReeves"];
+
+FindMinimum[eqn[1][[2]],{{X[1],0},{X[2],0}},
+	Method->{"Powell",
+		Method->{"Unimodal","MaxNarrowingIterations"->40}
+		}
+	];
+
+FindMinimum[eqn[1][[2]],{{X[1],0},{X[2],0}},Method->"IsaacNewton"];
+
 End[];
 
 EndPackage[];
