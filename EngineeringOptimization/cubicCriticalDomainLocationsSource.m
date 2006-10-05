562ace7e90d97c4fb270659969d6a0a44869cfe2
chris.chiasson <chris@chiasson.name>
2006-10-05 18:33:43 +0000

    can now create critical domain locations from a number of points greater than or equal to three; have only tried up to five

--- EngineeringOptimization/cubicCriticalDomainLocationsSource.m
+++ EngineeringOptimization/cubicCriticalDomainLocationsSource.m
@@ -1,34 +1,65 @@
-eqn[1]=Plus@@Table[Times[c[i],Power[x,i-1]],{i,4}]==y;
-eqn[2]=eqn[1]/.{x\[Rule]x[#],y\[Rule]y[#]}&/@Range[4];
-rep[1]=FullSimplify@Solve[eqn[2],Array[c,{4}]];
-rep[2]=FullSimplify@Solve[D[eqn[1],x],x];
+symbolRepFromIndexedPattern[pat_]=
+	Pattern[coord,pat][num_Integer]:>
+		ToExpression[SymbolName[coord]<>ToString[num]];
+createCriticalDomainLocations[y_Symbol,x_Symbol,c_Symbol,nPts_Integer]/;
+	nPts>=3:=
+	Module[{eqn,rep,i},
+		eqn[1]=Plus@@Table[Times[c[i],Power[x,i-1]],{i,nPts}]==y;
+		eqn[2]=eqn[1]/.{x->x[#],y->y[#]}&/@Range[nPts];
+		rep[1]=FullSimplify@Solve[eqn[2],Array[c,{nPts}]];
+		rep[2]=FullSimplify@Solve[D[eqn[1],x],x];
 (*the Simplify has been commented because it generates results outide the 
 bracket at MachinePrecision*)
-rep[3]=(*Simplify[*)rep[2]/.rep[1][[1]](*]*);
-xpr[1]=x/.rep[3]/.{Pattern[coord,x|y][num_]:>ToExpression[SymbolName[coord]<>
-	ToString[num]]};
+		rep[3]=(*Simplify[*)rep[2]/.rep[1][[1]](*]*);
+		x/.rep[3]/.symbolRepFromIndexedPattern[x|y]
+		];
+
 (*the following line is commented out because conflicts are unlikely in this
 context*)
 (*$ModuleNumber=10^10*$SessionID;*)
-xpr[2]=Experimental`OptimizeExpression[xpr[1]];
-Block[{Block,Set,CompoundExpression},
-	SetDelayed[
-		cubicCriticalDomainLocations[
-			y1_?NumericQ,
-			x1_?NumericQ,
-			y2_?NumericQ,
-			x2_?NumericQ,
-			y3_?NumericQ,
-			x3_?NumericQ,
-			y4_?NumericQ,
-			x4_?NumericQ
-			],
-		#]&@@xpr[2]
-	];
+
+criticalDomainLocationsPatterns[symbols__Symbol,
+	arguments:Except[_Symbol]..]:=
+	ReleaseHold[
+		Map[Pattern[#,_]?NumericQ&,
+			Hold[arguments]/.
+				symbolRepFromIndexedPattern[Alternatives[symbols]]
+			]
+		];
+
+storeCriticalDomainLocations[criticalDomainLocations_Symbol,
+	y_Symbol,
+	x_Symbol,
+	c_Symbol,
+	nPts_Integer]:=
+	Module[
+		{xpr=
+			{
+				Array[
+					criticalDomainLocationsPatterns[y,x,y[#],x[#]]&,
+					{nPts},
+					{1},
+					criticalDomainLocations
+					],
+				Experimental`OptimizeExpression[
+					createCriticalDomainLocations[y,x,c,nPts]
+					]
+				}
+			},
+		Block[{Block,Set,CompoundExpression},SetDelayed@@xpr]
+		];
+
+storeCriticalDomainLocations[criticalDomainLocations,y,x,c,#]&/@{3,4};
+
 (*the XML`DocBook` context is needed becuse it defines the InputDirectoryName[]
 function*)
 Needs["XML`DocBook`"];
-cubicOutFileName=ToFileName[InputDirectoryName[],
-	"cubicCriticalDomainLocations.m"];
-If[FileType[cubicOutFileName]==File,DeleteFile[cubicOutFileName]];
-Save[cubicOutFileName,cubicCriticalDomainLocations];
+
+criticalOutFileName=ToFileName[InputDirectoryName[],
+	"criticalDomainLocations.m"];
+
+If[FileType[criticalOutFileName]===File,DeleteFile[criticalOutFileName]];
+
+If[FileType@DirectoryName[criticalOutFileName]===Directory,
+	Save[criticalOutFileName,criticalDomainLocations]
+	];
