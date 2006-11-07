07f6ddf50dbb02510e824d1e5feed9b41bf86a66
chris.chiasson <chris@chiasson.name>
2006-11-07 00:56:01 +0000

    added several clairifying comments and the ex. 2-1 plot to go along with the already-created table

--- EngineeringOptimization/Documentation/PR1/PR1.m
+++ EngineeringOptimization/Documentation/PR1/PR1.m
@@ -6,10 +6,12 @@ BeginPackage["EngineeringOptimization`Documentation`PR1`",
 
 Begin["`Private`"];
 
-{Attributes[#]={NHoldAll},Format[#[i:__Integer|__Symbol]]=Subscript[#,i]}&/@
+{Attributes[#]={NHoldAll},Format[#[subs__]]=Subscript[#,subs]}&/@
 	{F,P,X};
 
 prefix="pr_1_";
+
+(*ptol example tolerance for line search appendix*)
 
 MakeBoxes[anAbscissa,_]="#";
 
@@ -17,9 +19,15 @@ pTolLab="pTolLab";
 
 eqn[pTolLab]=ptol==10^-accuracyGoal+Abs[anAbscissa]*10^-precisionGoal;
 
+(*line search first toy problem*)
+
 xMinusTenSquared="xMinusTenSquared";
 
+(*write out the equation*)
+
 eqn[xMinusTenSquared]=f[x]==(x-10)^2;
+
+(*export the equation*)
 
 (export[#1]=XMLDocument[prefix<>#1<>".xml",
 	DocBookInlineEquation[prefix<>#1,eqn[#1],SetIdAttribute->#2],
@@ -31,12 +39,17 @@ eqn[xMinusTenSquared]=f[x]==(x-10)^2;
 
 optimize="optimize_";
 
+(*find the optimum (note the delayed assignment)*)
+
 request[optimize<>xMinusTenSquared]:=
 	FindMinimum[
 		eqn[xMinusTenSquared][[2]],
 		{x,0,1},
 		Method->"Unimodal"
 		];
+
+(*export the commands that would be needed to tell mathematica to find the
+minimum*)
 
 export[optimize<>xMinusTenSquared]=
 	{ExportDelayed[
@@ -56,6 +69,8 @@ export[optimize<>xMinusTenSquared]=
 
 result="_result";
 
+(*export the result of finding the minimum*)
+
 export[optimize<>xMinusTenSquared<>result]=
 	{ExportDelayed[
 		ToFileName[
@@ -71,6 +86,8 @@ Numerical Optimization Techniques for
 Engineering Design
 by Garret Vanderplaats*)
 
+(*set up formatting for some more variables*)
+
 (MakeBoxes[#1,_]=#2)&@@@
 	{{SectionModulus,"I"},
 		{YoungsModulus,"E"},
@@ -79,25 +96,29 @@ by Garret Vanderplaats*)
 		{Pascal,"Pa"}
 		};
 
+(*these are for handling units symbolically during equation manipulation*)
+
 $Assumptions=#>0&/@{Newton,X,Meter,YoungsModulus,SectionModulus};
+
+(*a list of units of each variable in the problem*)
 
 $UnitList={{X,Meter},{F,Newton},{P,Newton}};
 
+(*make the variables "squirt out" their units*)
+
 deUnitizeVariablesRep=#->Times[##]&@@@$UnitList;
+
+(*make the variables "absorb" their units*)
 
 unitizeVariablesRep=#->Divide[##]&@@@$UnitList;
 
-dropSIUnitsRep={Meter->1,Newton->1};
+dropSIUnitsRep={Meter->1,Newton->1}; (*delete units*)
 
-numbersToMachinePrecisionRep=x_Real:>SetPrecision[x,MachinePrecision];
+cm=Centi*Meter; (*centimeter*)
 
-cm=Centi*Meter;
+GPa=Giga*Pascal; (*giga pascal*)
 
-GPa=Giga*Pascal;
-
-Function[Format[#[a_]]=Subscript[#,a]]/@{F};
-
-(*given values*)
+(*given values and equations*)
 
 rep[1]:={H->25 cm, L->250 cm, A->25 cm^2, SectionModulus->750 cm^4,
 	YoungsModulus->70 GPa};
@@ -164,18 +185,7 @@ export[ex21given]=XMLDocument[
 
 beamLoad[X_]=eqn[4][[2]]/.Rule@@eqn[3]/.rep[1]/.rep[2]//PiecewiseExpand;
 
-(*Plot the function and the 20000 N load line.*)
-
 loadLine=20000;
-
-gr[1]=Plot[{beamLoad[X]/.dropSIUnitsRep,loadLine},
-	{X,0,25/100},
-	PlotStyle->{Black,Red},
-	AxesLabel->{
-		SequenceForm[X," ","(",Meter,")"],
-		SequenceForm[P[X]," ","(",Newton,")"]
-		}
-	];
 
 (*Solve the problem in the standard Mathematica way for comparison with mine.*)
 
@@ -234,6 +244,11 @@ xpr[4]=FindMinimum[
 
 xprList[3]=xpr/@{2,3,4};
 
+(*we had to minimize the negative to get the maximum - and we had to minimize
+the absolute value of the difference to find the intersections -- to get the
+appropriate y coordinates for our x value solutions, the (real) function must be
+evaluated again at those x solutions*)
+
 xprList[4]=
 	MapThread[{#1,#2[[2]]}&,
 		{beamLoad[X]/.dropSIUnitsRep/.xprList[3][[All,2]],xprList[3]}
@@ -248,8 +263,10 @@ ex21FigandTabTitle=
 		" Example 2-1 Solution"}
 		];
 
+(*make a table showing the solution*)
+
 tab[1]={
-	{SequenceForm["Maximum ",P[X]],
+	{SequenceForm["Maximum ",P[X,max]],
 		SequenceForm[loadLine Newton," ",X[1]],
 		SequenceForm[loadLine Newton," ",X[2]]
 		},
@@ -259,6 +276,45 @@ tab[1]={
 	};
 
 tab[2]=Prepend[Transpose@tab[1],{"","Reference",Method->"\"Unimodal\""}];
+
+gprim[1]=
+	MapThread[
+		{Green,
+			PointSize[0.02],
+			Point[{X/.#2,#1}],
+			Black,
+			Text[#3,{X+#5/.#2,#1+#4}]
+			}&,
+		Fold[Append,
+			Transpose[xprList[4]],
+			{{P[X,max],P[X[1]],P[X[2]]},
+				Identity[Sequence][
+					{1,-1,-1}*#1/15,
+					{0,1,-1}*#2/15
+					]&@@Max/@
+						Transpose[
+							xprList[4]/.{X->num_}:>num
+							]
+				}
+			]
+		];
+
+(*Plot the function and the 20000 N load line.*)
+(*make a plot with the same data as the table*)
+
+gr[1]=Plot[{beamLoad[X]/.dropSIUnitsRep,loadLine},
+	{X,0,25/100},
+	PlotStyle->{Black,Red},
+	AxesLabel->{
+		SequenceForm[X," ","(",Meter,")"],
+		SequenceForm[P[X]," ","(",Newton,")"]
+		},
+	Epilog->{gprim[1]},
+	PlotRange->All,
+	ImageSize->$ExportWidth
+	];
+
+(*export the table*)
 
 ex21table="ex21table";
 
@@ -277,6 +333,29 @@ export[ex21table]=XMLDocument[
 		tab[2],
 		Caption->"The reference column is an exact solution to ten digits of "<>
 			"precision. The Unimodal column is my numeric solution.",
+		TitleAbbrev->"Ex. 2-1 Solution"
+		],
+	PrependDirectory->EODExportDirectory
+	];
+
+(*export the plot*)
+
+ex21plot="ex21plot"
+
+export[ex21plot]=XMLDocument[
+	prefix<>ex21plot<>".xml",
+	DocBookFigure[
+		prefix<>ex21plot,
+		ex21FigandTabTitle,
+		"A black line representing P[X] traces out a shape like a hill as X "<>
+			"increases to the right from zero. The red load line at 20000 N "<>
+			"crosses P[X] twice. The leftmost intersection is labeled P[X[1]]"<>
+			"The rightmost intersection is P[X[2]]. The maximum of P[X] is "<>
+			"labeled P[X,max] and its X value is roughly halfway between "<>
+			"X[1] and X[2]. Both intersections and the maximum are indicated "<>
+			"with a green dot.",
+		gr[1],
+		Caption->"The plot confirms the numerical solution.",
 		TitleAbbrev->"Ex. 2-1 Solution"
 		],
 	PrependDirectory->EODExportDirectory
