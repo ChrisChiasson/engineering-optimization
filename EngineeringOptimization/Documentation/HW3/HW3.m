b7429508466c32434735b48284e8f3b04d50810c
chris.chiasson <chris@chiasson.name>
2006-11-25 04:08:01 +0000

    added the export for the first tableau

--- EngineeringOptimization/Documentation/HW3/HW3.m
+++ EngineeringOptimization/Documentation/HW3/HW3.m
@@ -8,9 +8,43 @@ BeginPackage["EngineeringOptimization`Documentation`HW3`",
 
 Begin["`Private`"];
 
-{Attributes[#]={NHoldAll},Format[#[i_]]=Subscript[#,i]}&/@{X};
+{Attributes[#]={NHoldAll},Format[#[i_]]=Subscript[#,i]}&/@{X,excess,artificial};
+
+(MakeBoxes[#1,_]=#2)&@@@{{excess,"e"},{artificial,"a"}};
 
 prefix="hw_3";
+
+table[4,problem_,letter:a|e|f]:=
+	Prepend[tableau[4,problem,letter],{X@1,X@2,excess@1,excess@1,b}];
+
+table[4,problem_,letter:b|c|d]:=
+	Prepend[
+		tableau[4,problem,letter],
+		{X@1,X@2,excess@1,excess@1,artificial@1,artificial@2,b}
+		];
+
+tabtab="_table_tableau";
+
+exportTableau=Function[{title,tabl,titleAbbrev,caption},
+	With[{id="_"<>StringReplace[titleAbbrev," "->"_"]},
+		export[tabtab<>titleAbbrev]=
+			XMLDocument[
+				prefix<>id<>".xml",
+				DocBookTable[
+					prefix<>id,
+					title,
+"The columns in this table represent the coefficients of a polynomial. \
+The header in each column gives the variable corresponding to the \
+coefficients. The exception is the last column, labeled b. It is the right \
+hand side of the polynomial.",
+					tabl,
+					TitleAbbrev->titleAbbrev,
+					Caption->caption  
+					],
+				PrependDirectory->EODExportDirectory
+				]
+		]
+	];
 
 (*problem 4-4*)
 
@@ -24,14 +58,32 @@ eqns[4,4,4][X1_,X2_]=X1>=0;
 
 eqns[4,4,5][X1_,X2_]=X2>=0;
 
-tableau[4,4,a]={{2,1,-1,0,2},{2,4,0,-1,4},{2,4,0,0,F}};
-
 (*the excess variable coefficients are negative because one would have to
 subtract a positive quantity from the left hand sides of the constraints to
 have equality*)
 
+tableau[4,4,a]={{2,1,-1,0,2},{2,4,0,-1,4},{2,4,0,0,F}};
+
+With[{title="P 4-4 Initial Tableau"},
+  exportTableau[title,table[4,4,a],title,
+    XMLChain@XMLElement[
+        "para",{},{"The inequalities of problem 4 are converted to linear \
+equations by the use of excess variables, ",
+          ToXML@DocBookInlineEquation["",excess[i],SetIdAttribute->False],
+          ". There must be as many basis variables as there are constraint \
+equations. A basis variable is identified by the header of a column in which \
+only one row has a 1, with all other entries being 0 and with the the same \
+row having a 0 in all other basis columns. Since the constraint equations \
+can't easily be transformed into a canonical form with basis variables, \
+auxillary variables, ",
+          ToXML@DocBookInlineEquation["",artificial[i],SetIdAttribute->False],
+          ", must be added."}]]];
+
 tableau[4,4,b]=
 	{{2,1,-1,0,1,0,2},{2,4,0,-1,0,1,4},{2,4,0,0,0,0,F},{0,0,0,0,1,1,w}};
+
+(*row operations to eliminate artificial variables from the equation for
+w*)
 
 tableau[4,4,c]=
     ReplacePart[tableau[4,4,b],
@@ -68,7 +120,10 @@ Block[{$DisplayFunction=Identity},
           Apply[Sequence,#+{0,-0.02,0.02}&/@rangeSpec[4,4]],
           Fills->{White}]/.Line[__]->Sequence[]];
 
-Print@Context@LabelLines;
+(*ImplicitPlot isn't as good as using DrawGraphics to create a mesh the blocks
+the "non plotted" white area, because it can leave small seams in the white
+area. However, cutting the dependancy on DrawGraphics means I can redistribute
+the project more easily.*)
 
 gr[4,4,3]=
 	Show[gr[4,4,1],
@@ -104,9 +159,11 @@ gr[4,4,3]=
 					}
 				]
 			],
-		FrameLabel->X/@{1,2}];
+		FrameLabel->X/@{1,2},ImageSize->$ExportWidth
+		];
 
 (*create markup for the graph*)
+
 gr44="_gr_4_4";
 
 export[gr44]=
@@ -116,7 +173,7 @@ export[gr44]=
 function occur on a solid green line marking the edge of the domain. A large \
 green point at one end of the green line indicates the point the simplex \
 algorithm found. Dotted red lines show the other edges of the domain.",
-      gr[4,4,3]]];
+      gr[4,4,3]],PrependDirectory->EODExportDirectory];
 
 (*problem 4-5*)
 
@@ -144,11 +201,6 @@ tableau[4,5,e]=Delete[Map[Delete[#,5]&,tableau[4,5,d],1],-1];
 
 (*notice that the problem is already optimized, so this next call does \
 nothing*)
-
-tableau[4,5,f]=LinearMinimizeTableau[tableau[4,5,e],{{1,1},{2,4}}];
-
-(*notice that the problem is already optimized, 
-  so this next call does nothing*)
 
 tableau[4,5,f]=LinearMinimizeTableau[tableau[4,5,e],{{1,1},{2,4}}];
 
@@ -197,7 +249,7 @@ gr[4,5,3]=
 						]
 					}
 				],
-			FrameLabel->X/@{1,2}
+			FrameLabel->X/@{1,2},ImageSize->$ExportWidth
 			]
 		];
 
@@ -211,7 +263,7 @@ export[gr45]=
       "A contour plot is shown on a non-rectangular domain. A large green point \
 indicates the point that the simplex algorithm found to be the minimum on the \
 given domain. Dotted red lines show the other edges of the domain.",
-      gr[4,5,3]]];
+      gr[4,5,3]],PrependDirectory->EODExportDirectory];
 
 (*create xml for both sets of equations*)
 
@@ -236,6 +288,28 @@ Module[{id=ToString[SequenceForm@@BoxForm`Intercalate[{prefix,sol,##},"_"]]},
           DocBookInlineEquation[id,
             MatrixForm/@Equal@@Thread[sol[##][[2]],Rule]],
           PrependDirectory->EODExportDirectory]]&@@@{{4,4},{4,5}};
+
+filesToTransport={prefix<>"_screenshot_assignment.png"};
+
+If[EODExport===True,
+	Export@@@#&/@ReleaseHold@DownValues[export][[All,1]];
+		pwd=InputDirectoryName[];
+		CopyFile[
+			ToFileName[
+				pwd,
+				#
+				],
+			ToFileName[
+				EODExportDirectory,
+				#
+				],
+			Overwrite->True
+			]&/@filesToTransport;
+		CopyFile[InputFileName[],
+			ToFileName[EODExportDirectory,InputFileBaseName[]],
+			Overwrite->True
+			]
+	];
 
 End[];
 
