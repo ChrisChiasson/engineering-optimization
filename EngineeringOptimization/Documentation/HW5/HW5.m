6659a96f66b017ff4d70c37cdeb57df8af66bd5d
chris.chiasson <chris@chiasson.name>
2006-12-10 11:43:35 +0000

    added exports for all the equations given and requested in this homework added the tables + exports for showing the ALM iterations for problems 5-5 and 5-6

--- EngineeringOptimization/Documentation/HW5/HW5.m
+++ EngineeringOptimization/Documentation/HW5/HW5.m
@@ -47,7 +47,7 @@ allContours=Table[i*5,{i,1,15}];
 HW5ContourPlotOptions={ColorFunction->(Hue[.7,1-#,1]&),(*PlotPoints->200,*)
 	FrameLabel->varList,Contours->allContours,ImageSize->$ExportWidth};
 
-eqns[5,4,1][x1_,x2_]=F==(x1-1)^2+(x2-1)^2;
+eqns[5,_,1][x1_,x2_]=F==(x1-1)^2+(x2-1)^2;
 
 (*exact solution from Mathematica for the unconstrained problem*)
 sol[5,4,1]=Minimize[eqns[5,4,1][varSeq][[2]],varList];
@@ -76,6 +76,9 @@ penaltyTerms[5,4][extPenaltyMult_,intPenaltyMult_,transition_][x1_,x2_]=
 objective[5,4][extPenaltyMult_,intPenaltyMult_,transition_][x1_,x2_]=
 	eqns[5,4,1][x1,x2][[2]]+
 		penaltyTerms[5,4][extPenaltyMult,intPenaltyMult,transition][x1,x2];
+
+sol[5,4,1,rp_]:=sol[5,4,1,rp]=
+	Minimize[objective[5,4][whatever,rp,-2][varSeq],varList];
 
 (*export the objective function*)
 
@@ -134,19 +137,19 @@ gr[5,4,2,rp_]:=gr[5,4,2,rp]=firstContourLabels[gr[5,4,1,rp],
 
 (*label the minimum*)
 
-solutionLabel[solVector:{_?NumberQ,_?NumberQ},
-				labelOffset:{_?NumberQ,_?NumberQ}]:=
+solutionLabel[solValue_,solVector:{_,_},labelOffset:{_,_}]:=
 	Graphics@{Black,
 		Text[
 			DisplayForm@Cell[
-				StripBoxes@ToBoxes@sol[5,4,1][[1]],Background->White
+				StripBoxes@ToBoxes@N[solValue,2],Background->White
 				],
-			solVector,labelOffset
+			N@solVector,labelOffset
 			],
 		Green,PointSize[0.03],Point[solVector]
 		};
 
-gr[5,4,3,rp_]:=gr[5,4,3,rp]=solutionLabel[X/@Range@2/.sol[5,4,1][[2]],{-1,1}];
+gr[5,4,3,rp_]:=gr[5,4,3,rp]=
+	solutionLabel[sol[5,4,1,rp][[1]],sol[5,4,1,rp][[2,All,2]],{-1,1}];
 
 gr[5,4,4,rp_]:=gr[5,4,4,rp]=Show@@(gr[5,4,#,rp]&)/@Range@3;
 
@@ -165,33 +168,180 @@ gr[5,4,4,rp_]:=gr[5,4,4,rp]=Show@@(gr[5,4,#,rp]&)/@Range@3;
 				{"rpPrime",rpPrime,SetIdAttribute->False},
 				{"epsilon",\[Epsilon],SetIdAttribute->False},
 				{"rpPrime_Limit",rpPrime->Superscript[0,plus]},
-				{"epsilon_Limit",\[Epsilon]->Superscript[0,minus]}
+				{"epsilon_Limit",\[Epsilon]->Superscript[0,minus]},
+				{"rp_Equal_1",rp==1,SetIdAttribute->False},
+				Sequence@@({GenUC["lambda_Equal",#],\[Lambda]==#,
+							SetIdAttribute->False
+							}&/@{-4,-1,0,1,2})
 				};
 
 gr54="graph_5_4_";
 
 (*export the two problem 5-4 pseudo objective function contour plots*)
+(*************************************FIXME****************************************************)
+unboundContourPlotAltText="";
 
 (export[gr54<>#1]=
-        XMLDocument[prefix<>gr54<>#1<>".xml",
-          DocBookFigure[prefix<>gr54<>#1,
-            XMLChain@
-              XMLElement[
-                "phrase",{},{"P 5-4 Objective Function, \[CapitalPhi], with ",
-                  ToXML@rpPrimeXMLChain[#2]}],"",gr[5,4,4,#2]],
-          PrependDirectory->EODExportDirectory])&@@@{{rp1,1},{rpHalf,
-      1/2}}
+	XMLDocument[prefix<>gr54<>#1<>".xml",
+		DocBookFigure[prefix<>gr54<>#1,
+			XMLChain@XMLElement["phrase",{},
+				{"P 5-4 Objective Function, \[CapitalPhi], Contour Plot with ",
+					ToXML@rpPrimeXMLChain[#2]
+					}],
+			unboundContourPlotAltText,gr[5,4,4,#2]
+			],
+		PrependDirectory->EODExportDirectory
+		]
+	)&@@@{{rp1,1},{rpHalf,1/2}};
+
+(*export the analytical solutions to the minimization problems (these correspond
+to the graphs)*)
+
+sol54="solution_5_4_"
+
+With[{solEqn=Reduce[D[F==objective[5,4][whatever,#2,-2][varSeq],{varList,1}],
+				varList]
+		},
+	export[sol54<>#1]=
+		XMLDocument[prefix<>sol54<>#1<>".xml",
+			DocBookEquation[prefix<>sol54<>#1,
+				XMLChain[XMLElement["phrase",{},
+					{"P 5-4 Objective Function, \[CapitalPhi], Analytic",
+						" Solution with ",ToXML[rpPrimeXMLChain[#2]]
+						}
+					]],
+				DocBookEquationSequence[
+					\[CapitalPhi]==objective[5,4][whatever,#,-2][varSeq]/.
+						ToRules@solEqn,
+					Sequence@@solEqn
+					]
+				],
+			PrependDirectory->EODExportDirectory
+			]
+	]&@@@{{rp1,1},{rpHalf,1/2}};
+
+(*moving on to problem 5-5 & 5-6*)
+
+(*the constraint equations*)
+
+eqns[5,5,2][x1_,x2_]=x1-x2-2==0;
+
+eqns[5,6,2][x1_,x2_]=x1+x2-1/2<=0;
+
+(*inline exports of all given equations in this homework*)
+
+(export[prefix<>GenUC[eqns,##]]=
+	XMLDocument[prefix<>GenUC[eqns,##]<>".xml",
+		DocBookInlineEquation[prefix<>GenUC[eqns,##],eqns[##2][X@1,X@2]],
+		PrependDirectory->EODExportDirectory
+		])&@@@{{5,All,1},{5,4,3},Sequence@@({5,#,2}&/@Range[4,6])};
+
+(*the augmented lagrangian equations with rp = 1*)
+
+augLag="augmented_lagrangian";
+
+(export[GenUC[augLag,#1,#2]]=
+	XMLDocument[prefix<>GenUC[augLag,#1,#2]<>".xml",
+		DocBookEquation[prefix<>GenUC[augLag,#1,#2],
+			StringSequence["P ",#1,"-",#2," Augmented Lagrangian"],
+			Function[rp,\[CapitalPhi]==eqns[#1,#2,1][varSeq][[2]]+
+				EngineeringOptimization`Private`penalty[{eqns[#1,#2,2][varSeq]},
+					rp,{\[Lambda]},Method->"AugmentedLagrangeMultiplier"
+					]]/@DocBookEquationSequence[rp,1],
+			Caption->XMLElement["para",{},{"\[Lambda] is the Lagrange ",
+				"Multiplier. As requested, in the second equation ",
+				ToXML@DocBookInlineEquation[GenUC[eqns,#1,#2,"rp"],rp==1],"."}
+				]
+			],
+		PrependDirectory->EODExportDirectory]
+	)&@@@{{5,5},{5,6}};
+
+(*the requested tables*)
+
+(*trackALM pulls the data out of the trace by looking at the convergence tests*)
+
+Attributes@trackALM={HoldFirst};
+
+trackALM[call_,firstPart:All|{__Integer}:All]:=
+	Flatten[Trace[call,
+		xpr_EngineeringOptimization`Private`fMCommonConvergenceTest/;
+			Cases[
+				HoldComplete[xpr],
+				symb:Except[
+					EngineeringOptimization`Private`fMCommonConvergenceTest,
+					_Symbol
+					]/;StringMatchQ[SymbolName@Unevaluated@symb,
+										"penaltyMultiplier$"~~__
+										],
+				{0,Infinity}
+				]=!={}
+		]][[firstPart,1,4]]/.xpr_Rule:>xpr[[2]]/.{num_?NumberQ}:>num;
+
+(*labelRows is a function for labeling rows of trace data*)
+
+labelRows[mat:{{__}..}]:=
+	MapIndexed[Prepend[#1,If[#2[[1]]===1,"Init",#2[[1]]-1]]&,mat];
+
+(*almTable pretty prints the trace data in a nice table that could be shown with
+TableForm*)
+
+Attributes@almTable={HoldFirst};
+
+almTable[command_,lagrangeMultipliers:{__?NumberQ}]:=
+	Prepend[
+		Internal`BlockFlatten[
+			labelRows@
+				trackALM[
+					command,
+					Range@4
+					]/.{{x1_?NumberQ,x2_?NumberQ}:>
+							SequenceForm["(",NumberForm[x1,3],
+								",",NumberForm[x2,3],")"
+								],
+							GoldenRatio->N@GoldenRatio
+							}&/@lagrangeMultipliers,
+			{{1,2}}
+			],
+		{"Iteration, p",SequenceForm["Maximum Ordinates,\n(",X[1],",",X[2],")"],
+			SequenceForm["Penalty Scale\nFactor, ",rp],
+			"Lagrange\nMultiplier, \[Lambda]"
+			}
+		];
+
+(*generate and store the tables*)
+
+tab55="table_5_5";
+
+tab[55]=
+	almTable[
+		NMinimize[{eqns[5,5,1][varSeq][[2]],eqns[5,5,2][varSeq]},
+			{{X@1,-13,-11},{X@2,19,21}},
+			Method->{"AugmentedLagrangeMultiplier",
+				"InitialLagrangeMultipliers"->#
+				}],
+		{0,1,-4}
+		];
+
+tab56="table_5_6";
+
+tab[tab56]=
+	almTable[
+		NMinimize[{eqns[5,6,1][varSeq][[2]],eqns[5,6,2][varSeq]},
+			{{X@1,-13,-11},{X@2,19,21}},
+			Method->{"AugmentedLagrangeMultiplier",
+				"InitialLagrangeMultipliers"->#
+				}],
+		{0,2,-1}
+		];
 
 Abort[];
 
 (*constraint for problem 5-5*)
-eqn[3]=X[1]-X[2]-2==0;
 
 sol[2]=NMinimize[{eqn[1][[2]],eqn[3]},rng/@var@1,
 	Method->"AugmentedLagrangeMultiplier"]
 
 (*constraint for problem 5-6*)
-eqn[4]=X[1]+X[2]-(1/2)<=0;
 
 sol[3]=NMinimize[{eqn[1][[2]],eqn[4]},rng/@var@1,
 	Method->"AugmentedLagrangeMultiplier"]
