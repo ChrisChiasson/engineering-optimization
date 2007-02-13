7be1ca7969471b30557474fb0d062e1788fb4485
chris.chiasson <chris@chiasson.name>
2007-02-13 04:28:28 +0000

    flipped the order of loading pr2 and pr4 so that a crash does not happen in pr4, probably due to a bug in mathematica itself

--- EngineeringOptimization/Documentation/ExecuteAllDocumentation.m
+++ EngineeringOptimization/Documentation/ExecuteAllDocumentation.m
@@ -7,11 +7,14 @@ Begin["`Private`"];
 
 Get@"Utilities`CleanSlate`"
 
+(*original order {"HW1","PR1","HW2","HW3","PR2","HW4","HW5","PR4"}*)
 With[{eod="EngineeringOptimization`Documentation`"},
 	Get[eod<>#<>"`"];
-	CleanSlate[Evaluate[eod<>#<>"`"]]
-	]&/@
-	{"HW1","PR1","HW2","HW3","PR2","HW4","HW5","PR4"};
+	CleanSlate[Evaluate[eod<>#<>"`"]];
+	Update[];
+	Share[]
+	]&/@(*pr2 is the culprit*)
+	{"HW1","PR1","HW2","HW3","PR4","HW4","HW5","PR2"};
 
 If[EODExport===True,
 	eoSources=
