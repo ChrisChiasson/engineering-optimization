cfc0579069edbb18d99f6690f75115c472b5444f
chris.chiasson <chris@chiasson.name>
2007-02-28 22:55:57 +0000

    switched the package over to MMA v6 style

--- EngineeringOptimization/Documentation/HW3/HW3.m
+++ EngineeringOptimization/Documentation/HW3/HW3.m
@@ -1,35 +1,42 @@
+(* ::Package:: *)
+
 BeginPackage["EngineeringOptimization`Documentation`HW3`",
 	{"EngineeringOptimization`Documentation`",
 		"EngineeringOptimization`Documentation`Utility`",
 		"EngineeringOptimization`",
 		"Graphics`InequalityGraphics`",
 		"Graphics`FilledPlot`",
-		"XML`DocBook`","Utilities`FileHandling`"}];
+		"XML`DocBook`","Utilities`FileHandling`"}]
 
-Begin["`Private`"];
 
-{Attributes[#]={NHoldAll},Format[#[i_]]=Subscript[#,i]}&/@{X,excess,artificial};
+Begin["`Private`"]
 
-(MakeBoxes[#1,_]=#2)&@@@{{excess,"e"},{artificial,"a"}};
 
-prefix="hw_3_";
+{Attributes[#]={NHoldAll},Format[#[i_]]=Subscript[#,i]}&/@{X,excess,artificial}
+
+(MakeBoxes[#1,_]=#2)&@@@{{excess,"e"},{artificial,"a"}}
+
+
+prefix="hw_3_"
+
 
 table[4,problem_,letter:a|e|f]:=
-	Prepend[tableau[4,problem,letter],{X@1,X@2,excess@1,excess@2,b}];
+	Prepend[tableau[4,problem,letter],{X@1,X@2,excess@1,excess@2,b}]
 
 table[4,4,letter:b|c|d]:=
 	Prepend[
 		tableau[4,4,letter],
 		{X@1,X@2,excess@1,excess@2,artificial@1,artificial@2,b}
-		];
+		]
 
 table[4,5,letter:b|c|d]:=
 	Prepend[
 		tableau[4,5,letter],
 		{X@1,X@2,excess@1,excess@2,artificial@1,b}
-		];
+		]
 
-tabtab="table_tableau_";
+
+tabtab="table_tableau_"
 
 exportTableau=Function[{title,tabl,titleAbbrev,caption},
 	With[{id=StringReplace[title," "->"_"]},
@@ -52,23 +59,24 @@ hand side of the polynomial.",
 		]
 	];
 
+
 (*problem 4-4*)
+eqns[4,4,1][X1_,X2_]=F==2*X1+4*X2
 
-eqns[4,4,1][X1_,X2_]=F==2*X1+4*X2;
+eqns[4,4,2][X1_,X2_]=2*X1+X2>=2
 
-eqns[4,4,2][X1_,X2_]=2*X1+X2>=2;
+eqns[4,4,3][X1_,X2_]=2*X1+4*X2>=4
 
-eqns[4,4,3][X1_,X2_]=2*X1+4*X2>=4;
+eqns[4,4,4][X1_,X2_]=X1>=0
 
-eqns[4,4,4][X1_,X2_]=X1>=0;
+eqns[4,4,5][X1_,X2_]=X2>=0
 
-eqns[4,4,5][X1_,X2_]=X2>=0;
 
 (*the excess variable coefficients are negative because one would have to
 subtract a positive quantity from the left hand sides of the constraints to
 have equality*)
 
-tableau[4,4,a]={{2,1,-1,0,2},{2,4,0,-1,4},{2,4,0,0,F}};
+TableForm[tableau[4,4,a]={{2,1,-1,0,2},{2,4,0,-1,4},{2,4,0,0,F}}]
 
 excessXMLChain=
 	DocBookInlineEquation[prefix<>"excess",excess[i],
@@ -97,8 +105,10 @@ exportTableau[
 			]
 	];
 
-tableau[4,4,b]=
-	{{2,1,-1,0,1,0,2},{2,4,0,-1,0,1,4},{2,4,0,0,0,0,F},{0,0,0,0,1,1,w}};
+
+TableForm[tableau[4,4,b]=
+	{{2,1,-1,0,1,0,2},{2,4,0,-1,0,1,4},{2,4,0,0,0,0,F},{0,0,0,0,1,1,w}}
+	]
 
 exportTableau[
 	"P 4-4 Augmented Tableau",
@@ -118,20 +128,24 @@ exportTableau[
 			]
 	];
 
+
 (*row operations to eliminate artificial variables from the equation for
 w*)
 
-tableau[4,4,c]=
+TableForm[tableau[4,4,c]=
     ReplacePart[tableau[4,4,b],
       Last[tableau[4,4,b]]-tableau[4,4,b][[1]]-tableau[4,4,b][[2]],
-      Length[tableau[4,4,b]]];
+      Length[tableau[4,4,b]]]]
 
 exportTableau["P 4-4 Augmented Tableau in Canonical Form",
   table[4,4,c],
   "P 4-4 Canonical Augmented Tableau","Row reduction options are used to \
 bring the artifical variables into the basis."];
 
-tableau[4,4,d]=LinearMinimizeTableau[tableau[4,4,c],{{1,5},{2,6}}];
+
+TableForm[
+	tableau[4,4,d]=LinearMinimizeTableau[tableau[4,4,c],{{1,5},{2,6}}]
+	]
 
 exportTableau["P 4-4 Augmented Tableau After Optimization",
   table[4,4,d],
@@ -142,21 +156,24 @@ variables have been reduced to 0 and that adding them to the original \
 constraint equations does not change those equations. The artificial \
 variables and their objective sum may be removed."];
 
-tableau[4,4,e]=Delete[Map[Drop[#,{5,6}]&,tableau[4,4,d],1],-1];
+
+TableForm[tableau[4,4,e]=Delete[Map[Drop[#,{5,6}]&,tableau[4,4,d],1],-1]]
+
 
 (*notice that the problem is already optimized, 
   so this next call changes nothing*)
+TableForm[tableau[4,4,f]=
+	LinearMinimizeTableau[tableau[4,4,e],{{1,1},{2,2}}]
+	]
 
-tableau[4,4,f]=LinearMinimizeTableau[tableau[4,4,e],{{1,1},{2,2}}];
 
 (*graph soluton*)
-
 sol[4,4]=Minimize[{eqns[4,4,1][X@1,X@2][[2]],
-        eqns[4,4,#][X@1,X@2]&/@Range[2,5]},{X@1,X@2}];
+        eqns[4,4,#][X@1,X@2]&/@Range[2,5]},{X@1,X@2}]
+
 
 (*now that I have created the solution vector, I can write the final tableau
 for problem 4*)
-
 exportTableau["P 4-4 Canonical Form of Initial Tableau",
 	table[4,4,e],
 	"P 4-4 Canonical Initial Tableau",
@@ -181,14 +198,18 @@ exportTableau["P 4-4 Canonical Form of Initial Tableau",
 			]
 	];
 
-rangeSpec[4,4]={{X@1,X@1-2/.sol[4,4][[2]],X@1+2/.sol[4,4][[2]]},
-	{X@2,X@2-2/.sol[4,4][[2]],X@2+2/.sol[4,4][[2]]}};
 
-regionFunction[4,4][X1_,X2_]=eqns[4,4,#][X1,X2]&/@And@@Range[2,5];
+rangeSpec[4,4]={{X@1,X@1-2/.sol[4,4][[2]],X@1+2/.sol[4,4][[2]]},
+	{X@2,X@2-2/.sol[4,4][[2]],X@2+2/.sol[4,4][[2]]}}
+
+
+regionFunction[4,4][X1_,X2_]=eqns[4,4,#][X1,X2]&/@And@@Range[2,5]
+
 
 version6=If[$VersionNumber>=6,Identity[Sequence][##],Identity[Sequence][]]&
 
 version5=If[$VersionNumber<6,Identity[Sequence][##],Identity[Sequence][]]&
+
 
 Block[{$DisplayFunction=Identity},
     gr[4,4,1]=
@@ -202,13 +223,13 @@ Block[{$DisplayFunction=Identity},
     gr[4,4,2]=
       InequalityPlot[!regionFunction[4,4][X@1,X@2],
           Apply[Sequence,#+{0,-0.02,0.02}&/@rangeSpec[4,4]],
-          Fills->{White},PlotStyle->{White}]];
+          Fills->{White},PlotStyle->{White}]]
+
 
 (*ImplicitPlot isn't as good as using DrawGraphics to create a mesh that blocks
 the "non plotted" white area, because it can leave small seams in the white
 area. However, cutting the dependancy on DrawGraphics means I can redistribute
 the project more easily.*)
-
 gr[4,4,3]=
 	Show[gr[4,4,1],
 		version5@gr[4,4,2],
@@ -244,11 +265,11 @@ gr[4,4,3]=
 				]
 			],
 		FrameLabel->X/@{1,2},ImageSize->$ExportWidth
-		];
+		]
+
 
 (*create markup for the graph*)
-
-gr44="gr_4_4";
+gr44="gr_4_4"
 
 export[gr44]=
   XMLDocument[prefix<>gr44<>".xml",
@@ -268,29 +289,37 @@ algorithm found. Dotted red lines show the other edges of the domain.",
       	"labeled."
       ],PrependDirectory->EODExportDirectory];
 
+
 (*problem 4-5*)
+eqns[4,5,1][X1_,X2_]=F==2 X1+4 X2
 
-eqns[4,5,1][X1_,X2_]=F==2 X1+4 X2;
+eqns[4,5,2][X1_,X2_]=2 X1+X2>=2
 
-eqns[4,5,2][X1_,X2_]=2 X1+X2>=2;
+eqns[4,5,3][X1_,X2_]=2 X1+X2>=-1
 
-eqns[4,5,3][X1_,X2_]=2 X1+X2>=-1;
+eqns[4,5,4][X1_,X2_]=X1>=0
 
-eqns[4,5,4][X1_,X2_]=X1>=0;
+eqns[4,5,5][X1_,X2_]=X2>=0
 
-eqns[4,5,5][X1_,X2_]=X2>=0;
 
-tableau[4,5,a]={{2,1,-1,0,2},{2,1,0,-1,-1},{2,4,0,0,F}};
+TableForm[tableau[4,5,a]=
+	{{2,1,-1,0,2},{2,1,0,-1,-1},{2,4,0,0,F}}
+	]
 
 exportTableau["P 4-5 Initial Tableau",table[4,5,a],Automatic,None];
 
-tableau[4,5,b]={{2,1,-1,0,1,2},{-2,-4,0,1,0,1},{2,4,0,0,0,F},{0,0,0,0,1,w}};
+
+TableForm[tableau[4,5,b]=
+	{{2,1,-1,0,1,2},{-2,-4,0,1,0,1},{2,4,0,0,0,F},{0,0,0,0,1,w}}
+	]
 
 exportTableau["P 4-5 Augmented Tableau",table[4,5,b],Automatic,None];
 
-tableau[4,5,c]=
+
+TableForm[tableau[4,5,c]=
     ReplacePart[tableau[4,5,b],Last[tableau[4,5,b]]-tableau[4,5,b][[1]],
-      Length[tableau[4,5,b]]];
+      Length[tableau[4,5,b]]]
+	]
 
 exportTableau[
 	"P 4-5 Augmented Tableau in Canonical Form",
@@ -299,7 +328,10 @@ exportTableau[
 	None
 	];
 
-tableau[4,5,d]=LinearMinimizeTableau[tableau[4,5,c],{{1,5},{2,4}}];
+
+TableForm[tableau[4,5,d]=
+	LinearMinimizeTableau[tableau[4,5,c],{{1,5},{2,4}}]
+	]
 
 exportTableau[
 	"P 4-5 Augmented Tableau After Optimization",
@@ -308,17 +340,23 @@ exportTableau[
 	None
 	];
 
-tableau[4,5,e]=Delete[Map[Delete[#,5]&,tableau[4,5,d],1],-1];
+
+TableForm[tableau[4,5,e]=
+	Delete[Map[Delete[#,5]&,tableau[4,5,d],1],-1]
+	]
+
 
 (*notice that the problem is already optimized, so this next call does \
 nothing*)
+TableForm[tableau[4,5,f]=
+	LinearMinimizeTableau[tableau[4,5,e],{{1,1},{2,4}}]
+	]
 
-tableau[4,5,f]=LinearMinimizeTableau[tableau[4,5,e],{{1,1},{2,4}}];
 
 (*graph soluton*)
-
 sol[4,5]=Minimize[{eqns[4,5,1][X@1,X@2][[2]],
-      eqns[4,5,#][X@1,X@2]&/@Range[2,5]},{X@1,X@2}];
+      eqns[4,5,#][X@1,X@2]&/@Range[2,5]},{X@1,X@2}]
+
 
 exportTableau["P 4-5 Canonical Form of Initial Tableau",
 	table[4,5,e],
@@ -341,10 +379,13 @@ exportTableau["P 4-5 Canonical Form of Initial Tableau",
 			]
 	];
 
-rangeSpec[4,5]={{X@1,X@1-2/.sol[4,5][[2]],X@1+2/.sol[4,5][[2]]},{X@2,
-      X@2-2/.sol[4,5][[2]],X@2+2/.sol[4,5][[2]]}};
 
-regionFunction[4,5][X1_,X2_]=eqns[4,5,#][X1,X2]&/@And@@Range[2,5];
+rangeSpec[4,5]={{X@1,X@1-2/.sol[4,5][[2]],X@1+2/.sol[4,5][[2]]},{X@2,
+      X@2-2/.sol[4,5][[2]],X@2+2/.sol[4,5][[2]]}}
+
+
+regionFunction[4,5][X1_,X2_]=eqns[4,5,#][X1,X2]&/@And@@Range[2,5]
+
 
 Block[{$DisplayFunction=Identity},
     gr[4,5,1]=
@@ -358,7 +399,8 @@ Block[{$DisplayFunction=Identity},
     gr[4,5,2]=
       InequalityPlot[!regionFunction[4,5][X@1,X@2],
           Apply[Sequence,#+{0,-0.02,0.02}&/@rangeSpec[4,5]],
-          Fills->{White},PlotStyle->{White}]];
+          Fills->{White},PlotStyle->{White}]]
+
 
 gr[4,5,3]=
 	With[{solVector={X@1,X@2}/.sol[4,5][[2]]},
@@ -388,11 +430,11 @@ gr[4,5,3]=
 				],
 			FrameLabel->X/@{1,2},ImageSize->$ExportWidth
 			]
-		];
+		]
 
-gr45="gr_4_5";
 
 (*create markup for the graph*)
+gr45="gr_4_5"
 
 export[gr45]=
   XMLDocument[prefix<>gr45<>".xml",
@@ -407,8 +449,8 @@ given domain. Dotted red lines show the other edges of the domain.",
       ],
       PrependDirectory->EODExportDirectory];
 
-(*create xml for both sets of equations*)
 
+(*create xml for both sets of equations*)
 Outer[
 	With[
 		{id=prefix<>ToString[SequenceForm@@BoxForm`Intercalate[{eqns,##},"_"]]},
@@ -421,6 +463,7 @@ Outer[
 	{4,5},
 	Range[5]
 	];
+
 
 filesToTransport={prefix<>"screenshot_assignment.png"};
 
@@ -442,8 +485,10 @@ If[EODExport===True,
 			ToFileName[EODExportDirectory,InputFileBaseName[]],
 			Overwrite->True
 			]
-	];
+	]
 
-End[];
 
-EndPackage[];
+End[]
+
+
+EndPackage[]
